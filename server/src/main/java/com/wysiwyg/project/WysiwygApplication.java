6d501896ce0c94a5461ecd837aaf075d9207587a
Kang Changgu <rkckr1@naver.com>
2021-10-01 14:49:43 +0900

    SpringBoot 프로젝트 생성

---
+++ server/src/main/java/com/wysiwyg/project/WysiwygApplication.java
@@ -0,0 +1,13 @@
+package com.wysiwyg.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WysiwygApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WysiwygApplication.class, args);
+    }
+
+}
