dd2a917959f31bb25c7652aeffb0e5e0c5688226
Kang Changgu <rkckr1@naver.com>
2021-10-27 20:29:59 +0900

    model repository 내 torchvision fasterrcnn 모델 추가

---
+++ model/torch/model.py
@@ -0,0 +1,34 @@
+import torch
+import torchvision
+from PIL import Image
+
+
+class FasterRCNNWrapper(torch.nn.Module):
+
+	def __init__(self):
+		super().__init__()
+		self.model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
+
+	def forward(self, x):
+		output = self.model([x])
+
+		# eval mode
+		if len(output) == 1:
+			return output[0]["boxes"], output[0]["labels"], output[0]["scores"]
+		
+		# train mode
+		else:
+			loss, detection = output
+			return detection[0]['boxes'], detection[0]['labels'], detection[0]['scores']
+
+
+if __name__ == "__main__":
+	image = Image.open("./input/horses.jpg")
+	transformed = torchvision.transforms.ToTensor()(image)
+
+	model = FasterRCNNWrapper()
+	model.eval()
+	output = model(transformed)
+
+	script = torch.jit.script(model)
+	script.save('model.pt')
