9db2eda155e19208be318ace1cd26b5377a9a167
Kang Changgu <rkckr1@naver.com>
2021-11-13 20:36:30 +0900

    주석 제거

--- model/server/views/image_view.py
+++ model/server/views/image_view.py
@@ -14,27 +14,8 @@ def create_endpoints(app, services, config):
 
     @app.route("/images", methods=["POST"])
     def image():
-        # image_post = request.form.to_dict()
-        # image = request.files.get("image")
-
-        # image_post["imageFilename"] = save_image(image)
-        # image_service.save_image_post(image_post)
-
         image_post = request.form.to_dict()
         image_post["image"] = request.files.get("image")
         image_service.save_image_post(image_post)
 
         return jsonify()
-
-    # def save_image(image):
-
-    #     now = datetime.now()  # current date and time
-
-    #     extension = image.filename.split(".")[-1]
-    #     date_time = now.strftime("%Y%m%d_%H%M%S")
-    #     filename = date_time + "." + extension
-
-    #     path = os.path.join(image_url, filename)
-    #     image.save(path)
-
-    #     return filename
