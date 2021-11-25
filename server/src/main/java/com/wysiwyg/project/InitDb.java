b2aaa2109dc258ff20e89790e4eae59e17c74197
Kang Changgu <rkckr1@naver.com>
2021-11-25 16:37:37 +0900

    release 브랜치 수정사항 develop 브랜치 내 반영

--- server/src/main/java/com/wysiwyg/project/InitDb.java
+++ server/src/main/java/com/wysiwyg/project/InitDb.java
@@ -20,7 +20,7 @@ public class InitDb {
 
     @PostConstruct
     public void init() {
-//        initService.dbInit();
+        initService.dbInit();
     }
 
     @Component
--- server/src/test/java/com/wysiwyg/project/client/GithubClientTest.java
+++ server/src/test/java/com/wysiwyg/project/client/GithubClientTest.java
@@ -21,92 +21,92 @@ import java.util.stream.Collectors;
 @SpringBootTest
 public class GithubClientTest {
 
-//    @Test
-//    public void getGithubProfile() {
-//
-//        ObjectMapper objectMapper = new ObjectMapper();
-//        objectMapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
-//
-//        ExchangeStrategies exchangeStrategies = ExchangeStrategies.builder()
-//                .codecs(configurer ->
-//                        configurer.defaultCodecs().jackson2JsonDecoder(new Jackson2JsonDecoder(objectMapper)))
-//                .build();
-//
-//        WebClient webClient = WebClient.builder()
-//                .baseUrl("https://api.github.com/users/")
-//                .exchangeStrategies(exchangeStrategies)
-//                .build();
-//
-//        Mono<GithubProfile> githubProfileMono = webClient.get()
-//                .uri("/rxdcxdrnine")
-//                .retrieve()
-//                .bodyToMono(GithubProfile.class);
-//
-//        GithubProfile githubProfile = githubProfileMono.block();
-//        System.out.println(githubProfile);
-//    }
-//
-//    @Test
-//    public void getGithubPushes() {
-//
-//        ObjectMapper objectMapper = new ObjectMapper();
-//        objectMapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
-//        objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
-//
-//        ExchangeStrategies exchangeStrategies = ExchangeStrategies.builder()
-//                .codecs(configurer -> {
-//                    configurer.defaultCodecs().jackson2JsonDecoder(new Jackson2JsonDecoder(objectMapper));
-//                    configurer.defaultCodecs().maxInMemorySize(-1);
-//                })
-//                .build();
-//
-//        WebClient webClient = WebClient.builder()
-//                .baseUrl("https://api.github.com/users/")
-//                .exchangeStrategies(exchangeStrategies)
-//                .build();
-//
-//        Mono<GithubPushEvent[]> githubEventsMono = webClient.get()
-//                .uri("rxdcxdrnine/events")
-//                .retrieve()
-//                .bodyToMono(GithubPushEvent[].class);
-//
-//        GithubPushEvent[] githubEventsArray = githubEventsMono.block();
-//        List<GithubPushEvent> githubEvents = Arrays.stream(githubEventsArray)
-//                .filter(event -> event.getType().equals("PushEvent"))
-//                .collect(Collectors.toList());
-//
-//        for(GithubPushEvent githubEvent : githubEvents) {
-//            List<GithubPushCommit> githubCommits = Arrays.stream(githubEvent.getPayload().getCommits())
-//                    .collect(Collectors.toList());
-//
-//            for(GithubPushCommit githubCommit : githubCommits) {
-//                getGithubCommit(githubCommit.getUrl());
-//            }
-//        }
-//    }
-//
-//    @Test
-//    public void getGithubCommit(String githubCommitUrl) {
-//
-//        ObjectMapper objectMapper = new ObjectMapper();
-//        objectMapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
-//
-//        ExchangeStrategies exchangeStrategies = ExchangeStrategies.builder()
-//                .codecs(configurer -> {
-//                    configurer.defaultCodecs().jackson2JsonDecoder(new Jackson2JsonDecoder(objectMapper));
-//                })
-//                .build();
-//
-//        WebClient webClient = WebClient.builder()
-//                .exchangeStrategies(exchangeStrategies)
-//                .build();
-//
-//        Mono<GithubCommit> githubCommitMono = webClient.get()
-//                .uri(githubCommitUrl)
-//                .retrieve()
-//                .bodyToMono(GithubCommit.class);
-//
-//        GithubCommit githubCommit = githubCommitMono.block();
-//        System.out.println(githubCommit.getHtmlUrl());
-//    }
+    @Test
+    public void getGithubProfile() {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
+
+        ExchangeStrategies exchangeStrategies = ExchangeStrategies.builder()
+                .codecs(configurer ->
+                        configurer.defaultCodecs().jackson2JsonDecoder(new Jackson2JsonDecoder(objectMapper)))
+                .build();
+
+        WebClient webClient = WebClient.builder()
+                .baseUrl("https://api.github.com/users/")
+                .exchangeStrategies(exchangeStrategies)
+                .build();
+
+        Mono<GithubProfile> githubProfileMono = webClient.get()
+                .uri("/rxdcxdrnine")
+                .retrieve()
+                .bodyToMono(GithubProfile.class);
+
+        GithubProfile githubProfile = githubProfileMono.block();
+        System.out.println(githubProfile);
+    }
+
+    @Test
+    public void getGithubPushes() {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
+        objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+
+        ExchangeStrategies exchangeStrategies = ExchangeStrategies.builder()
+                .codecs(configurer -> {
+                    configurer.defaultCodecs().jackson2JsonDecoder(new Jackson2JsonDecoder(objectMapper));
+                    configurer.defaultCodecs().maxInMemorySize(-1);
+                })
+                .build();
+
+        WebClient webClient = WebClient.builder()
+                .baseUrl("https://api.github.com/users/")
+                .exchangeStrategies(exchangeStrategies)
+                .build();
+
+        Mono<GithubPushEvent[]> githubEventsMono = webClient.get()
+                .uri("rxdcxdrnine/events")
+                .retrieve()
+                .bodyToMono(GithubPushEvent[].class);
+
+        GithubPushEvent[] githubEventsArray = githubEventsMono.block();
+        List<GithubPushEvent> githubEvents = Arrays.stream(githubEventsArray)
+                .filter(event -> event.getType().equals("PushEvent"))
+                .collect(Collectors.toList());
+
+        for(GithubPushEvent githubEvent : githubEvents) {
+            List<GithubPushCommit> githubCommits = Arrays.stream(githubEvent.getPayload().getCommits())
+                    .collect(Collectors.toList());
+
+            for(GithubPushCommit githubCommit : githubCommits) {
+                getGithubCommit(githubCommit.getUrl());
+            }
+        }
+    }
+
+    @Test
+    public void getGithubCommit(String githubCommitUrl) {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
+
+        ExchangeStrategies exchangeStrategies = ExchangeStrategies.builder()
+                .codecs(configurer -> {
+                    configurer.defaultCodecs().jackson2JsonDecoder(new Jackson2JsonDecoder(objectMapper));
+                })
+                .build();
+
+        WebClient webClient = WebClient.builder()
+                .exchangeStrategies(exchangeStrategies)
+                .build();
+
+        Mono<GithubCommit> githubCommitMono = webClient.get()
+                .uri(githubCommitUrl)
+                .retrieve()
+                .bodyToMono(GithubCommit.class);
+
+        GithubCommit githubCommit = githubCommitMono.block();
+        System.out.println(githubCommit.getHtmlUrl());
+    }
 }
