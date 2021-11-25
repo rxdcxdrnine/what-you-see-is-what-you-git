12b53723772da524fb0456ff875916192f76de76
Kang Changgu <rkckr1@naver.com>
2021-11-25 10:20:24 +0900

    CORS 에러 해결을 위한 allowed origins 설정 변경

--- server/src/main/java/com/wysiwyg/project/config/WebConfig.java
+++ server/src/main/java/com/wysiwyg/project/config/WebConfig.java
@@ -24,7 +24,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5000")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST","PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true)
